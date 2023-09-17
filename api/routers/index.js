b6e6cfa84ea7b294465b5c9c0df6594873644b93
Ahmed BARGADY <bargadyahmed2000@gmail.com>
2023-09-17 18:14:16 +0100

    updating swagger

--- api/routers/index.js
+++ api/routers/index.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import locationRouter from "./location/index.js";
 // import chatRouter from "./chat/index.js";
 import orderRouter from "./order/index.js";
 import driverRouter from "./driver/index.js";
+import { SwaggerSpec } from "./swagger.js";
 
 const routers = Router();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 routers.use("/location", locationRouter);
 // routers.use("/chat", chatRouter);
@@ -15,6 +21,17 @@ routers.get("/", (req, res) => {
   return res.json({
     message: "Welcome to the PicckR Map API",
   });
+});
+
+routers.get("/swagger.json", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(SwaggerSpec);
+});
+
+routers.get("/assets/swagger.json", (req, res) => {
+  const jsonFilePath = path.join(__dirname, "../../assets", "swagger.json");
+  // Send the JSON file as a response
+  res.sendFile(jsonFilePath);
 });
 
 export default routers;
--- api/routers/swagger.js
+++ api/routers/swagger.js
@@ -1,16 +1,11 @@
 import swaggerUi from "swagger-ui-express";
 import swaggerJsdoc from "swagger-jsdoc";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
 import dotenv from "dotenv";
-import { Router } from "express";
 
-const swaggerRouter = Router();
 
 dotenv.config();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+
 
 const options = {
   definition: {
@@ -46,7 +41,7 @@ const options = {
 };
 
 
-const SwaggerSpec = swaggerJsdoc(options);
+export const SwaggerSpec = swaggerJsdoc(options);
 
 function swaggerDocs(app, port) {
   // swagger page
@@ -55,20 +50,6 @@ function swaggerDocs(app, port) {
     swaggerUi.serve,
     swaggerUi.setup(SwaggerSpec)
   );
-
-  // Save swagger docs .json
-  swaggerRouter.get("/swagger.json", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.send(SwaggerSpec);
-  });
-  
-  swaggerRouter.get("/assets/swagger.json", (req, res) => {
-    const jsonFilePath = path.join(__dirname, "../../assets", "swagger.json");
-    // Send the JSON file as a response
-    res.sendFile(jsonFilePath);
-  });
-
-  app.use("/swagger", swaggerRouter);
 
   console.log(`Swagger Docs available at http://localhost:${port}/docs`);
 }
