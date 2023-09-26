1fff89df048dbd29a69840e2ff253e5c6079d62e
Ahmed BARGADY <bargadyahmed2000@gmail.com>
2023-09-26 03:41:24 +0100

    testing socket io

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import http from "http";
-// import { Server } from "socket.io";
+import sockertIO from "socket.io";
 import routers from "./routers/index.js";
 import swaggerDocs from "./routers/swagger.js";
 // import { sendMessage } from "./routers/chatroom/mutation.js";
@@ -13,14 +13,13 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.MODE || "PROD";
 
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: "*",
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = sockertIO(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
 
 app.use(
   cors({
@@ -34,25 +33,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", routers);
 
-// io.on("connection", (socket) => {
-//   console.log("A user connected, socket id: ", socket.id);
+io.on("connection", (socket) => {
+  console.log("A user connected, socket id: ", socket.id);
 
-//   socket.on("disconnect", () => {
-//     console.log("A user disconnected, socket id: ", socket.id);
-//   });
+  socket.on("disconnect", () => {
+    console.log("A user disconnected, socket id: ", socket.id);
+  });
+});
 
-//   // Handle custom events here
-//   socket.on("send-message", async (messageData) => {
-//     try {
-//       let message = await sendMessage(messageData);
-//       io.to(messageData.chatRoomId).emit("new-message", message);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   });
-// });
-
-server.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} in ${MODE} mode`);
   swaggerDocs(app, PORT);
 });
